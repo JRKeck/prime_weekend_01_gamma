20bf45a49c1f07c12f15efa05092c9e2477c3cd9
JRKeck <joshrkeck@gmail.com>
2015-07-18 16:58:49 -0500

    Add Alphabetical insertion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var totalSalary = 0;
 	var employeeSalary = 0;
+	var rowCounter = 0;
 	console.log(totalSalary);
 
 	//Place form inputs into formObj and call addRow
@@ -17,35 +18,53 @@ $(document).ready(function() {
 		$( ".employee-form" ).each(function(){
 		    this.reset();
 		});
-		addRow(formObj);
+		if (rowCounter <= 0) {
+			addRow(formObj);
+		}
+		else {
+			addRowAlpha(formObj);
+		}
 	});
 	//Remove a row by clicking on x
 	$("body").on('click', '.remove-row', function() {
 		$(this).closest('tr').remove();
 		employeeSalary = $(this).parent().prev().text();
 		redeuceSalary(employeeSalary);
+		rowCounter--;
 	});
 	//Adds a row to the list
 	function addRow(formObj) {
 		$(".results tbody").append("<tr><td>"+formObj.fname+"</td><td>"+formObj.lname+"</td><td>"+formObj.empnum+"</td><td>"+formObj.title+"</td><td data-review='"+formObj.review+"'>"+formObj.review+"</td><td>"+formObj.salary+"</td><td><button class='remove-row btn-danger btn-xs'><i class='fa fa-times'></i></button></td></tr>");
 		addSalary(formObj.salary);
+		rowCounter++;
+	}
+	//Adds a row in alphabetical order
+	function addRowAlpha(formObj) {
+		var added;
+		$('.employee-table > tbody  > tr').each(function() {
+			if ($(this).children().first().next().text() > formObj.lname) {
+				$(this).before("<tr><td>"+formObj.fname+"</td><td>"+formObj.lname+"</td><td>"+formObj.empnum+"</td><td>"+formObj.title+"</td><td data-review='"+formObj.review+"'>"+formObj.review+"</td><td>"+formObj.salary+"</td><td><button class='remove-row btn-danger btn-xs'><i class='fa fa-times'></i></button></td></tr>");
+				added = true;
+			}
+
+		});
+		if (!added) {$(".results tbody").append("<tr><td>"+formObj.fname+"</td><td>"+formObj.lname+"</td><td>"+formObj.empnum+"</td><td>"+formObj.title+"</td><td data-review='"+formObj.review+"'>"+formObj.review+"</td><td>"+formObj.salary+"</td><td><button class='remove-row btn-danger btn-xs'><i class='fa fa-times'></i></button></td></tr>");
+}
+	
+		rowCounter++;
 	}
 	//increase total salary
 	function addSalary(salary) {
-		salary = parseToNum(salary);
+		salary = parseInt(salary);
 		totalSalary += salary;
 		$('.total-comp-num').text(totalSalary);
 	}
 	//decrease total salary
 	function redeuceSalary(salary) {
-		salary = parseToNum(salary);
+		salary = parseInt(salary);
 		totalSalary -= salary;
 		$('.total-comp-num').text(totalSalary);
 	}
-	//parese to num
-	function parseToNum(num) {
-		num = parseInt(num);
-		return num;
-	}
+
 
 });
